3e2a48952f8434b432940a11b6e1e882ea43616c
rodrigo <rodrigovmoraes@gmail.com>
2022-01-21 14:59:04 -0300

    Life cycle demo bean and pet services moved to com.springframework.pets

--- src/main/java/com/springframework/pets/DogPetService.java
+++ src/main/java/com/springframework/pets/DogPetService.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgdi.services;
+package com.springframework.pets;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/28/19.
  */
-@Service("dog")
+@Profile({"dog", "default"})
+@Service
 public class DogPetService implements PetService {
     public String getPetType(){
         return "Dogs are the best!";
